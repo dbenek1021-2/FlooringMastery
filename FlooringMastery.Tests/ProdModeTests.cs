f05add37b811d205344807383f6a6c8c4d1aa8c4
Danielle (Dani) Benek <dbenek1021@gmail.com>
2019-10-24 15:18:53 -0400

    Add files via upload

---
+++ FlooringMastery.Tests/ProdModeTests.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlooringMastery.BLL;
+using FlooringMastery.Data;
+using FlooringMastery.Data.Repositories;
+using FlooringMastery.Models;
+using FlooringMastery.Models.Responses;
+using NUnit.Framework;
+
+namespace FlooringMastery.Tests
+{
+	public class ProdModeTests
+	{
+		private string _orderPath = ConfigurationManager.AppSettings["OrderFolder"].ToString();
+		private string _pathForTest = ConfigurationManager.AppSettings["OrderFile"].ToString();
+		private string _seedForTest = ConfigurationManager.AppSettings["SeedFile"].ToString();
+		private string _productPath = ConfigurationManager.AppSettings["ProductFile"].ToString();
+		private string _taxPath = ConfigurationManager.AppSettings["TaxFile"].ToString();
+
+		[SetUp]
+		public void Setup()
+		{
+			if (File.Exists(_pathForTest))
+			{
+				File.Delete(_pathForTest);
+			}
+
+			File.Copy(_seedForTest, _pathForTest);
+		}
+
+		[Test]
+		public void CanDisplayOrder()
+		{
+			OrderRepository repo = new OrderRepository();
+
+			List<Order> orders = repo.ReadOrders("06012013");
+
+			Assert.AreEqual(1, orders.Count());
+
+			Order check = orders[0];
+
+			Assert.AreEqual("06012013", check.OrderDate);
+			Assert.AreEqual(1, check.OrderNumber);
+			Assert.AreEqual("Wise", check.CustomerName);
+			Assert.AreEqual("Wood", check.ProductType);
+			Assert.AreEqual(1051.88, check.Total);
+		}
+
+		[Test]
+		public void CanAddOrder()
+		{
+			OrderRepository repo = new OrderRepository();
+
+			List<Order> orders = repo.ReadOrders("06012013");
+
+			Order addOrder = new Order();
+
+			addOrder.OrderDate = "06012013";
+			addOrder.OrderNumber = 2;
+			addOrder.CustomerName = "Acme";
+			addOrder.State = "OH";
+			addOrder.TaxRate = 6.25M;
+			addOrder.ProductType = "Wood";
+			addOrder.Area = 100.00M;
+			addOrder.CostPerSquareFoot = 5.15M;
+			addOrder.LaborCostPerSqareFoot = 4.75M;
+			addOrder.MaterialCost = 515.00M;
+			addOrder.LaborCost = 475.00M;
+			addOrder.Tax = 61.88M;
+			addOrder.Total = 1051.88M;
+			orders.Add(addOrder);
+
+			repo.OverwriteFile(orders, addOrder.OrderDate);
+
+			Assert.AreEqual(2, orders.Count());
+
+			Order check = orders[1];
+
+			Assert.AreEqual("06012013", check.OrderDate);
+			Assert.AreEqual("Acme", check.CustomerName);
+			Assert.AreEqual(1051.88M, check.Total);
+		}
+
+		[Test]
+		public void CanEditOrder()
+		{
+			OrderRepository repo = new OrderRepository();
+
+			List<Order> orders = repo.ReadOrders("06012013");
+
+			Order editOrder = orders[1];
+			editOrder.CustomerName = "TNT";
+
+			repo.OverwriteFile(orders, editOrder.OrderDate);
+
+			Assert.AreEqual(2, orders.Count());
+
+			Order check = orders[1];
+
+			Assert.AreEqual("06012013", check.OrderDate);
+			Assert.AreEqual("TNT", check.CustomerName);
+			Assert.AreEqual(1051.88M, check.Total);
+		}
+
+		[Test]
+		public void LoadProduct()
+		{
+			ProductRepository repo = new ProductRepository();
+			Product product = repo.LoadProduct("Carpet");
+
+			product.ProductType = "Carpet";
+			product.CostPerSquareFoot = 2.25M;
+			product.LaborCostPerSqareFoot = 2.10M;
+
+			Product actual = repo.LoadProduct("Carpet");
+
+			Assert.AreEqual("Carpet", actual.ProductType);
+			Assert.AreEqual(2.25M, actual.CostPerSquareFoot);
+			Assert.AreEqual(2.10M, actual.LaborCostPerSqareFoot);
+		}
+
+		[Test]
+		public void LoadTaxes()
+		{
+			TaxRepository repo = new TaxRepository();
+			Taxes tax = repo.LoadTaxes("OH");
+
+			tax.StateAbbreviation = "OH";
+			tax.StateName = "Ohio";
+			tax.TaxRate = 6.25M;
+
+			Taxes actual = repo.LoadTaxes("OH");
+
+			Assert.AreEqual("OH", actual.StateAbbreviation);
+			Assert.AreEqual("Ohio", actual.StateName);
+			Assert.AreEqual(6.25M, actual.TaxRate);
+		}
+	}
+}
---
+++ FlooringMastery.Tests/TestModeTests.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlooringMastery.BLL;
+using FlooringMastery.Data;
+using FlooringMastery.Data.Repositories;
+using FlooringMastery.Models;
+using FlooringMastery.Models.Responses;
+using NUnit.Framework;
+using System.Configuration;
+using System.IO;
+
+namespace FlooringMastery.Tests
+{
+	[TestFixture]
+	public class TestModeTests
+	{
+		[TestCase("06062019")]
+		public void CanDisplayOrder(string orderDate)
+		{
+			OrderManager manager = OrderManagerFactory.Create();
+
+			DisplayOrderResponse response = manager.DisplayOrders(orderDate);
+
+			Assert.IsNotNull(response.Order);
+			Assert.IsTrue(response.Success);
+			Assert.AreEqual(orderDate, response.Order.OrderNumber);
+		}
+
+		[TestCase("06062019", 1, "Acme", "OH", "6.25", "Wood", "100", "5.15", "4.75", "515.00", "475.00", "61.88", "1051.88", true)]
+		public void CanAddOrder(string orderDate, int orderNumber, string customerName, string state, decimal taxRate, string product,
+								decimal area, decimal costPerFt, decimal laborPerFt, decimal matCost, decimal laborCost, decimal tax,
+								decimal total, bool expectedResult)
+
+		{
+			OrderManager manager = OrderManagerFactory.Create();
+			Order newOrder = new Order();
+			newOrder.OrderDate = orderDate;
+			newOrder.OrderNumber = orderNumber;
+			newOrder.CustomerName = customerName;
+			newOrder.State = state;
+			newOrder.TaxRate = taxRate;
+			newOrder.ProductType = product;
+			newOrder.Area = area;
+			newOrder.CostPerSquareFoot = costPerFt;
+			newOrder.LaborCostPerSqareFoot = laborPerFt;
+			newOrder.MaterialCost = matCost;
+			newOrder.LaborCost = laborCost;
+			newOrder.Tax = tax;
+			newOrder.Total = total;
+
+			AddOrderResponse response = manager.AddOrder(newOrder, orderDate);
+
+			Assert.IsNotNull(response.NewOrder);
+			Assert.IsTrue(response.Success);
+			Assert.AreEqual(orderNumber, expectedResult);
+		}
+
+		[TestCase("06062019", 1, "Acme", "OH", 6.25, "Wood", 100, 5.15, 4.75, 515.00, 475.00, 61.88, 1051.88, true)]
+		public void CanEditOrder(string orderDate, int orderNumber, string customerName, string state, decimal taxRate, string product,
+								decimal area, decimal costPerFt, decimal laborPerFt, decimal matCost, decimal laborCost, decimal tax,
+								decimal total, bool expectedResult)
+
+		{
+			OrderManager manager = OrderManagerFactory.Create();
+			Order editOrder = new Order();
+			editOrder.OrderDate = orderDate;
+			editOrder.OrderNumber = orderNumber;
+			editOrder.CustomerName = customerName;
+			editOrder.State = state;
+			editOrder.TaxRate = taxRate;
+			editOrder.ProductType = product;
+			editOrder.Area = area;
+			editOrder.CostPerSquareFoot = costPerFt;
+			editOrder.LaborCostPerSqareFoot = laborPerFt;
+			editOrder.MaterialCost = matCost;
+			editOrder.LaborCost = laborCost;
+			editOrder.Tax = tax;
+			editOrder.Total = total;
+
+			EditOrderResponse response = manager.EditOrder(orderDate, orderNumber);
+
+			Assert.IsNotNull(response.Order);
+			Assert.AreEqual(response.Success, expectedResult);
+		}
+
+		[TestCase("Laminate", 1.75, 2.10, true)]
+		public void LoadProduct(string productType, decimal costPerFt, decimal laborPerFt, bool expectedResult)
+		{
+			ProductRepository repo = new ProductRepository();
+			Product product = new Product();
+
+			product.ProductType = productType;
+			product.CostPerSquareFoot = costPerFt;
+			product.LaborCostPerSqareFoot = laborPerFt;
+
+			Product actual = repo.LoadProduct(productType);
+
+			Assert.AreEqual(productType, actual.ProductType);
+			Assert.AreEqual(costPerFt, actual.CostPerSquareFoot);
+			Assert.AreEqual(laborPerFt, actual.LaborCostPerSqareFoot);
+		}
+
+		[TestCase("OH", "Ohio", 6.25, true)]
+		public void LoadTaxes(string stateAbb, string stateName, decimal taxRate, bool expectedResult)
+		{
+			TaxRepository repo = new TaxRepository();
+			Taxes tax = new Taxes();
+
+			tax.StateAbbreviation = stateAbb;
+			tax.StateName = stateName;
+			tax.TaxRate = taxRate;
+
+			Taxes actual = repo.LoadTaxes(stateAbb);
+
+			Assert.AreEqual(stateAbb, actual.StateAbbreviation);
+			Assert.AreEqual(stateName, actual.StateName);
+			Assert.AreEqual(taxRate, actual.TaxRate);
+		}
+	}
+}
